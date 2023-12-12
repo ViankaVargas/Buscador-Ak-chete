917aab87da191f2f1b80ca465139658b13e0deee
Vianka <vi.vargasmora@gmail.com>
2023-12-12 10:26:51 -0600

    Primera Version

---
+++ js/db.js
@@ -0,0 +1,355 @@
+const autos = [
+	{
+		marca: 'Iphone',
+		modelo: '8 Plus',
+		year: 2020,
+		precio: 30000,
+		puertas: 6,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: 'X',
+		year: 2020,
+		precio: 30000,
+		puertas: 1,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: 'XS',
+		year: 2020,
+		precio: 30000,
+		puertas: 8,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: 'XR',
+		year: 2020,
+		precio: 30000,
+		puertas: 13,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11',
+		year: 2020,
+		precio: 30000,
+		puertas: 51,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '12',
+		year: 2020,
+		precio: 30000,
+		puertas: 50,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '12 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 24,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '12 mini',
+		year: 2020,
+		precio: 30000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '13',
+		year: 2020,
+		precio: 30000,
+		puertas: 26,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14',
+		year: 2020,
+		precio: 30000,
+		puertas: 17,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15',
+		year: 2020,
+		precio: 30000,
+		puertas: 16,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 30,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 49,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15 Plus',
+		year: 2020,
+		precio: 30000,
+		puertas: 11,
+		color: 'Blanco',
+		transmision: 'Estuche original'
+	},
+	//
+	{
+		marca: 'Iphone',
+		modelo: '8',
+		year: 2020,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: 'XR',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11',
+		year: 2020,
+		precio: 30000,
+		puertas: 23,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '12 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 8,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '12 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '11 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '13',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '13 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 6,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14',
+		year: 2020,
+		precio: 30000,
+		puertas: 16,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Pro Max',
+		year: 2020,
+		precio: 30000,
+		puertas: 22,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '14 Plus',
+		year: 2020,
+		precio: 30000,
+		puertas: 20,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15',
+		year: 2020,
+		precio: 30000,
+		puertas: 10,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Iphone',
+		modelo: '15 Pro',
+		year: 2020,
+		precio: 30000,
+		puertas: 9,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Samsung',
+		modelo: 'A34',
+		year: 2020,
+		precio: 30000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Samsung',
+		modelo: 'A24',
+		year: 2020,
+		precio: 30000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Samsung',
+		modelo: 'A14',
+		year: 2020,
+		precio: 30000,
+		puertas: 14,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Samsung',
+		modelo: 'Honor X8',
+		year: 2020,
+		precio: 30000,
+		puertas: 1,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+	{
+		marca: 'Samsung',
+		modelo: 'A54',
+		year: 2020,
+		precio: 30000,
+		puertas: 9,
+		color: 'Blanco',
+		transmision: 'Estuche space'
+	},
+
+];
